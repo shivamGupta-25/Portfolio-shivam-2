5a84a28b58f4955349f5e6400b0f5e0707a20f72
Shivam Raj Gupta <guptashivam25oct@gmail.com>
2026-06-28 20:51:53 +0530

    updated svg

--- src/components/ui/svgs/pandas.tsx
+++ src/components/ui/svgs/pandas.tsx
@@ -1,14 +1,36 @@
 import type { SVGProps } from "react";
 
 const Pandas = (props: SVGProps<SVGSVGElement>) => (
-  <svg {...props} viewBox="0 0 64 64" fill="none">
-    <rect x="16" y="8" width="8" height="14" rx="1" fill="#130754" />
-    <rect x="16" y="26" width="8" height="6" rx="1" fill="#FFCA00" />
-    <rect x="16" y="36" width="8" height="14" rx="1" fill="#130754" />
-    <rect x="28" y="18" width="8" height="28" rx="1" fill="#130754" />
-    <rect x="40" y="14" width="8" height="14" rx="1" fill="#130754" />
-    <rect x="40" y="32" width="8" height="6" rx="1" fill="#FFCA00" />
-    <rect x="40" y="42" width="8" height="14" rx="1" fill="#130754" />
+  <svg
+    {...props}
+    viewBox="0 0 128 128"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M48.697 15.176h12.25v25.437h-12.25zM48.697 67.427h12.25v25.436h-12.25z"
+      fill="#130754"
+    />
+    <path
+      d="M48.697 48.037h12.25v12.001h-12.25z"
+      fill="#FFCA00"
+    />
+    <path
+      d="M29.017 36.087h12.25v84.552h-12.25z"
+      fill="#130754"
+    />
+    <path
+      d="M67.97 88.414h12.25v25.436H67.97zM67.97 36.117h12.25v25.437H67.97z"
+      fill="#130754"
+    />
+    <path
+      d="M67.97 68.983h12.25v12.001H67.97z"
+      fill="#E70488"
+    />
+    <path
+      d="M87.238 8.55h12.25v84.552h-12.25z"
+      fill="#130754"
+    />
   </svg>
 );
 
